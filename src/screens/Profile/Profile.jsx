4a91b98a6dbc803d56fce70544f49ece56cc78b5
Evelin Romero <Evelinrocio2816@gmail.com>
2023-10-12 00:05:56 -0300

    add Camera

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,27 +1,26 @@
-import { Text, View ,Image, Pressable} from 'react-native'
-import React, { useState } from 'react'
-import { Header } from '../../components'
-import { styles } from './Profile.styles'
-import * as ImagePicker from 'expo-image-picker'
-import { useDispatch ,useSelector} from 'react-redux'
-import { setCameraImage } from '../../features/Auth/authSlice'
-
+import { Text, View, Image, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Header } from "../../components";
+import { styles } from "./Profile.styles";
+import * as ImagePicker from "expo-image-picker";
+import { useDispatch, useSelector } from "react-redux";
+import { setCameraImage } from "../../features/Auth/authSlice";
 
 const Profile = () => {
-    const image = useSelector(state => state.auth.imageCamera)
-    
-    const dispatch=useDispatch()
+  const image = useSelector((state) => state.auth.imageCamera);
 
-    const verifyCameraPermissions = async () => {
-        const {granted}= await ImagePicker.requestCameraPermissionsAsync()
-        if (!granted) {
-            return false
-          }
-          return true
-      }
-    
-      const pickImage = async () => {
-          const isCameraOk = await verifyCameraPermissions()
+  const dispatch = useDispatch();
+
+  const verifyCameraPermissions = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) {
+      return false;
+    }
+    return true;
+  };
+
+  const pickImage = async () => {
+    const isCameraOk = await verifyCameraPermissions();
 
     if (isCameraOk) {
       let result = await ImagePicker.launchCameraAsync({
@@ -30,38 +29,46 @@ const Profile = () => {
         aspect: [9, 16],
         base64: true,
         quality: 0.4,
-      })
+      });
       if (!result.canceled) {
-        console.log(result.assets)
+        console.log(result.assets);
         dispatch(
-          setImage(`data:image/jpeg;base64,${result.assets[0].base64}`)
-        )
+          setCameraImage(`data:image/jpeg;base64,${result.assets[0].base64}`)
+        );
       }
     }
-      }
-    
-      const confirmImage = () => {
-        dispatch(setCameraImage(images))
-      }
+  };
 
-  return (<>
-    <View>
-        <Header title={'Profile'}/>
+  const confirmImage = () => {};
 
-    </View>
-    <View style={styles.container}>
-      {image ? null: <Image source={{
-        uri:'https://static.vecteezy.com/system/resources/previews/024/293/032/non_2x/illustration-of-user-icon-in-gray-color-vector.jpg'
-      }} style={styles.image}
-      resizeMode='cover'/>}
-     
-     <Pressable style={styles.profileCamera} onPress={pickImage}>
-        <Text>Tomar Foto De Perfil</Text>
-     </Pressable>
-    </View>
+  return (
+    <>
+      <View>
+        <Header title={"Profile"} />
+      </View>
+      <View style={styles.container}>
+        {image ?  <Image
+            source={{
+              uri: image,
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          /> : (
+          <Image
+            source={{
+              uri: "https://static.vecteezy.com/system/resources/previews/024/293/032/non_2x/illustration-of-user-icon-in-gray-color-vector.jpg",
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        )}
+
+        <Pressable style={styles.profileCamera} onPress={pickImage}>
+          <Text>Tomar Foto De Perfil</Text>
+        </Pressable>
+      </View>
     </>
-  )
-}
+  );
+};
 
-export default Profile
-
+export default Profile;
